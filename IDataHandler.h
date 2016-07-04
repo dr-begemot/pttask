c5ce931a4f71b5c16e327400bad3702b3381a635
dr_begemot <dr_begemot>
2016-07-04 13:51:44 +0300

    add IDataHandler class

---
+++ IDataHandler.h
@@ -0,0 +1,15 @@
+//
+//  IDataHandler.h
+//  pttask
+//
+
+#ifndef IDataHandler_h
+#define IDataHandler_h
+
+class IDataHandler {
+public:
+	virtual void handle_binary_data(const char* data, size_t size);
+	virtual void handle_text_data(const char* data, size_t size);
+};
+
+#endif /* IDataHandler_h */
