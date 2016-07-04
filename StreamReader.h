6c8e584a4b8ae66f5b4a776f3dde7f710bd28762
dr-begemot <okunev.g.a@gmail.com>
2016-07-04 16:50:19 +0300

    Update StreamReader.h

--- StreamReader.h
+++ StreamReader.h
@@ -7,15 +7,20 @@
 #define StreamReader_h
 
 #include <vector>
+
 #include "IStreamReader.h"
+#include "IDataHandler.h"
 
 #define USUAL_SIZE 4096
 
 class StreamReader: public IStreamReader {
 public:
+    StreamReader(std::shared_ptr<IDataHandler> h): handler(h){};
+    
     virtual int receive(const char* stream, size_t bytes_read);
 protected:
-    std::vector<char> v;
+    std::vector<char> buffer;
+    std::shared_ptr<IDataHandler> handler;
 };
 
 #endif /* StreamReader_h */
