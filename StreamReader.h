bcd60dc0a527e2416e15b627477ca28aa966768a
dr-begemot <okunev.g.a@gmail.com>
2016-07-06 11:46:09 +0300

    Update StreamReader.h

--- StreamReader.h
+++ StreamReader.h
@@ -15,7 +15,7 @@
 
 class StreamReader: public IStreamReader {
 public:
-    StreamReader(std::shared_ptr<IDataHandler> h): handler(h){};
+    StreamReader(std::shared_ptr<IDataHandler> &ph): handler(ph){};
     
     virtual int receive(const char* stream, size_t bytes_read);
 protected:
