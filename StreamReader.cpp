023e9d9b308ec95d342764efe3f4e661394bf69a
dr_begemot <dr_begemot>
2016-07-06 17:48:09 +0300

    th
    Signed-off-by: dr_begemot <dr_begemot>

--- StreamReader.cpp
+++ StreamReader.cpp
@@ -7,7 +7,21 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "StreamReader.h"
-
+int StreamReader::receive_via_string(const char *stream, size_t bytes_read){
+    size_t npos=0;
+    uint32_t packlen;
+    std::string s(stream, bytes_read);
+    std::string::iterator it=str.begin();
+    while(it<=str.end()){
+        if(*it==StartSymbol){
+            packlen = reinterpret_cast<uint32_t*>(&stream[i+1]);
+            if(i+packlen<=bytes_read){
+                handler->handle_binary_data(&s.c_str()[npos+1+sizeof(uint32_t)], packlen);
+                npos+=packlen;
+            }
+        }
+    }
+}
 int StreamReader::receive(const char *stream, size_t bytes_read){
     std::string s()
     size_t strstart;
--- StreamReader.h
+++ StreamReader.h
@@ -20,6 +20,7 @@ public:
     StreamReader(std::shared_ptr<IDataHandler> &ph): handler(ph){};
     
     virtual int receive(const char* stream, size_t bytes_read);
+    int receive_via_string(const char *stream, size_t bytes_read);
 protected:
     std::vector<char> buffer;
     std::shared_ptr<IDataHandler> handler;
